facfaf674c7c550d4671f15526d7b4168b539b3e
ahamadey27 <hamadey@gmail.com>
2023-10-29 15:55:30 -0400

    Added Java into HTML exercise

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,1 +1,6 @@
-alert("Push to close. Push it.");
+function My_First_Function() 
+	{
+		var String = "Kiss me, I'm Irish!"; 
+		var result = String.fontcolor("green"); 
+		document.getElementById("Irish").innerHTML = result; 
+	}
